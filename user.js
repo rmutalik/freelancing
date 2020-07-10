d9abfaef05596a35c9dd3c611eaffdcc7e9a79bc
Rohan Mutalik <rmutalik@gmail.com>
2020-07-09 23:39:57 -0400

    Dynamically created HTML elements for stories
    Also updated homepage links.

--- user.js
+++ user.js
@@ -7,17 +7,61 @@ function User(name, handle, hasStory, isFollowing) {
 	this.isFollowing = isFollowing;
 }
 
+function addStory(user) {
+	// account_picture
+	let node = document.createElement("img");
+	node.setAttribute("src", "./account_pictures/" + user.handle + ".png");
+
+	// wrap 'img' with 'a' element
+	let link = document.createElement('a');
+  link.innerHTML = node.outerHTML;
+  link.setAttribute('href', '#');
+  
+  document.getElementById("stories").appendChild(link);
+
+	// handle
+	node = document.createElement("p");
+	let textnode = document.createTextNode(user.handle);
+	node.appendChild(textnode);
+	document.getElementById("stories").appendChild(node);
+}
+
+function setStyle( objId, propertyObject )
+{
+ let elem = document.getElementById(objId);
+ for (let property in propertyObject)
+    elem.style[property] = propertyObject[property];
+}
+
 
 let users = [];
 
-let user1 = new User("Daud Naz", "daud.naz", false, true);
-let user2 = new User("Thejas Prasad", "nathesh92", true, true);
-let user3 = new User("Chirag Sakhuja", "chirag_sakhuja", false, true);
-let user4 = new User("Dustyn", "dustyndream", true, true);
-let user5 = new User("Nikhil Dixit", "nikhildixit26", false, true);
-let user6 = new User("Shikha Singh Hanspal", "shikhanspal", false, false);
-let user7 = new User("Chris Bumstead", "cbum", true, false);
+let user1 = new User("US National Whitewater Center", "usnwc", true, true);
+let user2 = new User("Metallica", "metallica", false, true);
+let user3 = new User("Tom Platz", "tomplatz", false, false);
+let user4 = new User("Chris Bumstead", "cbum", true, false);
+let user5 = new User("LeBron James", "kingjames", true, true);
+let user6 = new User("Artemus Dolgin", "artemusdolgin", true, true);
+let user7 = new User("Google", "google", true, true);
 
 users.push(user1, user2, user3, user4, user5, user6, user7);
 
 console.log(users);
+
+for(i=0; i<users.length; i++) {
+	if (users[i].isFollowing == true && users[i].hasStory == true) {
+		// console.log(users[i].handle);
+		addStory(users[i]);
+	}
+}
+
+let style = document.createElement('style');
+style.innerHTML = 
+	'#stories p {' + 
+		'display: inline-block; ' + 
+		'float: left; ' + 
+		'padding: 10px; '
+	'}';
+
+let ref = document.querySelector('script');
+ref.parentNode.insertBefore(style, ref);

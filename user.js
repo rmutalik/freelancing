a7ef8d1da446878b6c03ec879109f1ee50e189ba
Rohan Mutalik <rmutalik@gmail.com>
2020-07-20 01:53:00 -0400

    Finished suggestions box

--- user.js
+++ user.js
@@ -30,11 +30,58 @@ function addStory(user, i) {
 	document.querySelectorAll("#stories div")[i].appendChild(node);
 }
 
-function setStyle( objId, propertyObject )
+/*function setStyle( objId, propertyObject )
 {
  let elem = document.getElementById(objId);
  for (let property in propertyObject)
     elem.style[property] = propertyObject[property];
+}*/
+
+function viewSuggestions(user, i) {
+	// wrap with 'div' element
+	let div = document.createElement("div");
+	document.getElementById("suggestions").appendChild(div);
+
+	// account_picture
+	let node = document.createElement("img");
+	node.setAttribute("src", "./suggestions/" + user.handle + ".png");
+
+	// wrap 'img' with 'a' element
+	let link = document.createElement("a");
+  link.innerHTML = node.outerHTML;
+  link.setAttribute('href', '#');
+  link.className = "floatleft";
+  document.querySelectorAll("#suggestions div")[i].appendChild(link);	
+
+	// handle
+	node = document.createElement("p");
+	let textnode = document.createTextNode(user.handle);
+	node.appendChild(textnode);
+  node.style.color = "black";	
+  node.style.fontWeight = "500";
+
+	link = document.createElement("a");
+  link.innerHTML = node.outerHTML;
+  link.setAttribute('href', '#');
+	document.querySelectorAll("#suggestions div")[i].appendChild(link);	
+
+	// follow link
+	link = document.createElement("a");
+  link.innerHTML = "Follow";
+  link.setAttribute('href', '#');
+  link.className = "floatright";
+  link.style.color = "deepskyblue";
+  link.style.fontWeight = "bold";
+  link.style.fontSize = "12px";
+ 	document.querySelectorAll("#suggestions div")[i].appendChild(link);	
+
+ 	// add line breaks
+ 	let br = document.createElement("br");
+ 	document.querySelectorAll("#suggestions div")[i].appendChild(br);	
+ 	br = document.createElement("br");
+ 	document.querySelectorAll("#suggestions div")[i].appendChild(br);	
+ 	br = document.createElement("br");
+ 	document.querySelectorAll("#suggestions div")[i].appendChild(br); 	
 }
 
 
@@ -88,3 +135,32 @@ style.innerHTML =
 // Insert the style above before the page is loaded
 let ref = document.querySelector('script');
 ref.parentNode.insertBefore(style, ref);
+
+
+
+// Suggested users
+let suggested_users = [];
+
+let suggested_user1 = new User("Toby Abraham", "toabrahamby", false, false);
+let suggested_user2 = new User("Brandon Shaw", "branded_humor", false, false);
+let suggested_user3 = new User("carl hedfalk", "synfalc", false, false);
+let suggested_user4 = new User("TahiraT", "tahirataqi", false, false);
+let suggested_user5 = new User("jonni", "cancelmyuber", true, false);
+
+suggested_users.push(suggested_user1, suggested_user2, suggested_user3, suggested_user4, suggested_user5);
+
+console.log(suggested_users);
+
+
+// If you are not following users, show in your suggestions box
+new_users2 = [];
+for(i=0; i<suggested_users.length; i++) {
+	if (suggested_users[i].isFollowing == false) {
+		// console.log(users[i].handle);
+		new_users2.push(suggested_users[i]);
+	}
+}
+
+for(i=0; i<new_users2.length; i++) {
+	viewSuggestions(new_users2[i], i);
+}

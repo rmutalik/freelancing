51c6a65f8982f641edf4b7fad60191928020160b
Rohan Mutalik <rmutalik@gmail.com>
2020-07-19 23:53:41 -0400

    Added Javascript to create a post dynamically

---
+++ post.js
@@ -0,0 +1,13 @@
+// post.js
+
+function Post(user, location, picture, caption, timestamp) {
+	this.user = user;
+	this.location = location;
+	this.picture = picture;
+	this.caption = caption;
+	this.timestamp = timestamp;
+}
+
+function createPost() {
+	
+}
